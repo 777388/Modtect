95b6652426c374176af7d15a43079c42fe90eb13
Christopher Lycan <96343159+777388@users.noreply.github.com>
2023-09-26 03:52:50 -0700

    Update antimod.py

--- antimod.py
+++ antimod.py
@@ -1,10 +1,10 @@
 import sys
-print("usage: python3 antimod.py multiple leftovers")
+print("usage: python3 antimod.py limiter leftovers multiple")
 
 antimod = []
 for x in range(int(sys.argv[1])):
     antimod.append(x)
-for v in range(int(sys.argv[1])):  
+for v in range(int(sys.argv[3])):  
     F = lambda f: f * v + int(sys.argv[2])
 
 print(list(map(F, antimod)))
