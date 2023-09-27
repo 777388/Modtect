18477b925be07f7fa4d238a5118b85e232dce758
Christopher Lycan <96343159+777388@users.noreply.github.com>
2023-09-27 01:09:56 -0700

    Update antimod.py

--- antimod.py
+++ antimod.py
@@ -1,10 +1,12 @@
 import sys
+import math
 print("usage: python3 antimod.py multiple leftovers limiter")
 
 antimod = []
-for x in range(int(sys.argv[1])):
-    antimod.append(x)
+for x in range(1, int(sys.argv[1])):
+    antimod.append(abs(x))
 for v in range(int(sys.argv[3])):  
-    F = lambda f: f * v + int(sys.argv[2])
+    F = lambda f: f * v + int(sys.argv[2]); lambda f: math.sqrt(f*(f/f)+(f-f))**2%(f+1)
 
+print(abs(int(sys.argv[2])))
 print(list(map(F, antimod)))
